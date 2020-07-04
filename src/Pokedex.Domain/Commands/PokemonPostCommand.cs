9e4f7e53f0e3fb71924ed174dd0570f071a006ed
Guilherme Ortiz <code@guilhermeortiz.net>
2020-07-04 17:43:51 -0300

    Basic CRUD

--- src/Pokedex.Domain/Commands/PokemonPostCommand.cs
+++ src/Pokedex.Domain/Commands/PokemonPostCommand.cs
@@ -1,15 +1,16 @@
-using System;
+using MediatR;
+using Pokedex.Domain.Dtos.Pokemon;
+using Pokedex.Domain.Entities;
 
-namespace Pokedex.Domain.Dtos.Repository
+namespace Pokedex.Domain.Commands
 {
-    public class UpdatePokemonRepositoryDto
+    public class PokemonPostCommand : IRequest<ServiceResponse<GetPokemonDto>>
     {
+        
         public int Number { get; set; }
         public string Name { get; set; }
         public int Order { get; set; }
         public int Height { get; set; }
         public int Weight { get; set; }
-        public DateTime DateCreated { get; set; }
-        //public List<string> Types { get; set; }
     }
 }
--- src/Pokedex.Domain/Dtos/Repository/GetPokemonRepositoryDto.cs
+++ src/Pokedex.Domain/Dtos/Repository/GetPokemonRepositoryDto.cs
@@ -2,14 +2,15 @@ using System;
 
 namespace Pokedex.Domain.Dtos.Repository
 {
-    public class AddPokemonRepositoryDto
+    public class GetPokemonRepositoryDto
     {
         public int Number { get; set; }
         public string Name { get; set; }
         public int Order { get; set; }
         public int Height { get; set; }
         public int Weight { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
         //public List<string> Types { get; set; }
     }
 }
